f38f9b7fdc44f5835d4fcd770a2e8494aca599a0
Cleanjohn Dev <dev@cleanjohn.com>
2026-04-18 09:30:32 +0100

    Move login endpoint to top level of api directory for Vercel auto-detection

--- frontend/api/index.ts
+++
@@ -1,8 +0,0 @@
-const { VercelRequest, VercelResponse } = require('@vercel/node');
-const { createApp } = require('./bundle.js');
-
-const app = createApp();
-
-module.exports = async (req, res) => {
-  return app(req, res);
-};
